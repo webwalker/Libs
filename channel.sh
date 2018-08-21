1865d1e0cf70ac3e95b960d77b6447eccf83f7d8
Xu Jian <xujian.sh@qq.com>
2018-08-21 10:58:24 +0800

    sh

--- channel.sh
+++ channel.sh
@@ -1,5 +1,5 @@
 #apk
-java -jar /data/app/walle-cli-all.jar batch -f channel qutoutiao.apk 
+java -jar /data/app/walle-cli-all.jar batch -f channel.txt qutoutiao.apk 
 mv qutoutiao.apk qutoutiao_d001.apk
 tar zcvf download.tar.gz *.apk
 
